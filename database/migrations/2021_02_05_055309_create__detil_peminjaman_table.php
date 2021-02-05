15d147efcf10e0db7827a65e52b6e35e3c3208f7
vincentyonathan <vincentyonathan24@gmail.com>
2021-02-05 13:05:30 +0700

    add new database

---
+++ database/migrations/2021_02_05_055309_create__detil_peminjaman_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetilPeminjamanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_detil_peminjaman', function (Blueprint $table) {
+            $table->bigIncrements('iD_DetilPeminjaman');
+            $table -> unsignedBigInteger('NISN_Buku');
+            $table -> foreign('NISN_Buku')-> references ('ISBN/NISN') -> on ('_buku');
+            $table -> string ('Status_Buku');
+            #$table -> unsignedBigInteger('iD_Manager');
+            #$table -> foreign ('iD_Manager') -> references ('iD_Manager')-> on ('_manager');
+            $table -> unsignedBigInteger('iD_Peminjaman');
+            $table -> foreign ('iD_Peminjaman') -> references ('iD_Peminjaman')-> on ('_peminjaman');
+            $table -> string ('Kondisi_BukuAwal');
+            $table -> string ('Kondisi_BukuAkhir');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_detil_peminjaman');
+    }
+}
---
+++ database/migrations/2021_02_05_060214_create_peminjaman_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeminjamanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table-> bigIncrements('iD_Peminjaman');
+            #$table -> unsignedBigInteger('iD_Pelanggan');
+            #$table -> foreign ('iD_Pelanggan') -> references ('iD_Pelanggan')-> on ('pelanggan');
+            #$table -> unsignedBigInteger('iD_Pegawai');
+            #$table -> foreign ('iD_Pegawai') -> references ('iD_Pegawai')-> on ('pegawai');
+            $table -> date('Tanggal_Peminjaman');
+            $table -> date('Tanggal_JatuhTempo');
+            $table -> date('Tanggal_Pengembalian');
+            $table -> float('Denda_Peminjaman');
+            $table -> float('Denda_Kerusakan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+}
