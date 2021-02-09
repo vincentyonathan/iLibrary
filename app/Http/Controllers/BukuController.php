dc94a59d362d82a1388e015ae63a1bd81ce2917f
vincentyonathan <vincentyonathan24@gmail.com>
2021-02-09 11:50:48 +0700

    controller

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Buku;
 
 class BukuController extends Controller
 {
-    //
+    public function index()
+    {
+        $buku = Buku::orderBy('ISBN/NISN')-> get();
+
+        return view('#nama_view_buku', []);
+    }
+
+    public function show($id)
+    {
+        return view('nama_detil view',[
+            'ISBN/NISN' => $buku
+        ]);
+    }
+    
 }
